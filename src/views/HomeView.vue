12e6beb483edab81b1af5ba1c9981b51202f14bf
“muddyrain” <qiu3291002845@gmail.com>
2023-04-03 17:00:28 +0800

    feat: 141 pointer events 穿透事件问题

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="home">
     <Scene />
+    <BigScreen />
   </div>
 </template>
 
 <script setup>
 import Scene from "@/components/scene.vue";
+import BigScreen from "@/components/BigScreen.vue";
 </script>
