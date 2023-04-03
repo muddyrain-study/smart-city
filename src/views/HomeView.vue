0cc38f972b240bebd9ff55f40afa0b30540c371e
“muddyrain” <qiu3291002845@gmail.com>
2023-04-03 17:54:47 +0800

    feat: 获取请求事件

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,4 +8,16 @@
 <script setup>
 import Scene from "@/components/scene.vue";
 import BigScreen from "@/components/BigScreen.vue";
+import { onMounted, reactive } from "vue";
+import { getSmartCityList } from "@/api/api";
+
+const dataInfo = reactive({});
+
+onMounted(async () => {
+  const res = await getSmartCityList();
+  dataInfo.iot = res.data.iot;
+  dataInfo.event = res.data.event;
+  dataInfo.power = res.data.power;
+  dataInfo.test = res.data.test;
+});
 </script>
